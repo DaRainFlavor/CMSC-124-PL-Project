914826dc8a1887126b5f41cb3f74dff1a7cbf14e
DaRainFlavor <adrianvaflor8@gmail.com>
2024-10-17 06:38:39 +0800

    Simplified the structure, fixed glitches excpt flash, organized the pictures, added themes, and terminal works

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-# This covers the main where customization should preferrably be only the title and window icon
-# Anything here reflects the entire application
+import customtkinter
+from IDE import *
 
-import tkinter as tk
-from main_window import MainWindow
 
-def main():
-  root = tk.Tk()
-  root.title("Compiley Studio")
-  window = MainWindow(root)
-  root.iconbitmap("icon.ico")
-  root.mainloop()
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("dark-blue")
 
-if __name__ == "__main__":
-  main()
+# setup
+root = customtkinter.CTk()
+root.title("Compiley Studio")
+root.iconbitmap("icon.ico")
+
+# create frames
+itemFrame = customtkinter.CTkFrame(master=root)
+itemFrame.grid(row=0, column=0, sticky = "nsew")
+
+# Configure root to expand
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+IDE_instance = IDE(itemFrame, root)
+
+root.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-
-    # self.main.master.overrideredirect(True)
-    # self.after(3000, self.switchToSecondWindow)
+Compiley Studio is a compiler for BrainRot (.rot) files.\nBrainRot is a programming language whose syntax is\ninspired by Gen-Z terms and slangs. This preserves the\ncringe of the generation this is built.
