62150cfbeb06dea6332c984e1f50900c3728dcb2
DaRainFlavor <adrianvaflor8@gmail.com>
2024-10-17 08:29:43 +0800

    Asks save changes when closing

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import customtkinter
 from IDE import *
+from tkinter import messagebox
+
+
+def on_closing():
+  # Ask the user if they want to close the window
+  if (not IDE_instance.welcome and not IDE_instance.flash) and IDE_instance.default_text != IDE_instance.scroll.get(1.0, tk.END)[:-1]:
+    response = msgbox.askyesnocancel("Unsaved Changes", f"Do you want to save changes to {os.path.basename(IDE_instance.filepath)}?")
+    if response is None:  # Cancel
+      return
+    elif response:  # Yes, save changes
+      IDE_instance.save_file()
+  root.destroy()  # Close the window
 
 
 customtkinter.set_appearance_mode("light")
@@ -19,5 +31,7 @@ root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
 IDE_instance = IDE(itemFrame, root)
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
 root.mainloop()
