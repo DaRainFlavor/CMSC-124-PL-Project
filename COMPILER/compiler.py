8cc03e314d55b4d58496ebf3ba06f0bee2a99255
leidc024 <lidelacruz1@up.edu.ph>
2024-12-05 01:13:36 +0800

    undo the last push it leads to more errors my bad

--- COMPILER/compiler.py
+++ COMPILER/compiler.py
@@ -521,8 +521,7 @@ class Compiler:
     if self.currentToken == token:
       self.currentLexeme = self.currentToken = ""
       self.currentLexemeToken()
-      if self.currentToken != ";":  # Check for semicolon at the end
-            self.debugMissingSemicolon()
+      
     else:
       raise SyntaxError(f"Skibidi in Toilet {self.getLineError()}: Expected {token} but found {self.currentToken}")
 ############# SYMBOL TABLE #############
@@ -983,9 +982,7 @@ class Compiler:
     #   else: break
 
     # return line_error
-  def debugMissingSemicolon(self):
-    # If the expected semicolon is not found
-    raise SyntaxError(f"Skibidi in Toilet {self.getLineError()}: Expected SEMICOLON at the end of the statement.")
+  
 
 
   def debugMissingDataType(self):
